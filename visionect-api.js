c0d1d337f09388a25db339401b1ebd07e87de5b0
longchiwen <bostjan.jelen@visionect.com>
2019-10-09 11:03:05 +0200

    Added protocol option

--- visionect-api.js
+++ visionect-api.js
@@ -1,151 +1,205 @@
 var visionectApi = {
-    getDevice: getDevice,
-    getDeviceList: getDeviceList,
-    updateDevice: updateDevice,
-    updateDeviceList: updateDeviceList,
-    removeDevice: removeDevice,
-    rebootDevice: rebootDevice,
+  getDevice: getDevice,
+  getDeviceList: getDeviceList,
+  updateDevice: updateDevice,
+  updateDeviceList: updateDeviceList,
+  removeDevice: removeDevice,
+  rebootDevice: rebootDevice,
 
-    getSession: getSession,
-    getSessionList: getSessionList,
-    updateSession: updateSession,
-    updateSessionList: updateSessionList,
-    removeSession: removeSession,
-    createSession: createSession,
-    restartSession: restartSession,
-    restartSessionList: restartSessionList,
+  getSession: getSession,
+  getSessionList: getSessionList,
+  updateSession: updateSession,
+  updateSessionList: updateSessionList,
+  removeSession: removeSession,
+  createSession: createSession,
+  restartSession: restartSession,
+  restartSessionList: restartSessionList,
 
-    getUser: getUser,
-    getUserList: getUserList,
-    updateUser: updateUser,
-    updateUserList: updateUserList,
-    createUser: createUser,
-    removeUser: removeUser,
+  getUser: getUser,
+  getUserList: getUserList,
+  updateUser: updateUser,
+  updateUserList: updateUserList,
+  createUser: createUser,
+  removeUser: removeUser,
 
-    getVisionectConfig: getVisionectConfig,
-    updateConfig: updateConfig,
+  getVisionectConfig: getVisionectConfig,
+  updateConfig: updateConfig,
 
-    getServerLiveView: getServerLiveView,
-    getDeviceLiveView: getDeviceLiveView,
+  getServerLiveView: getServerLiveView,
+  getDeviceLiveView: getDeviceLiveView,
 
-    getDeviceStatus: getDeviceStatus,
+  getDeviceStatus: getDeviceStatus,
 
-    sendImage: sendImage,
+  sendImage: sendImage,
 
-    setConfig: setConfig
-}
+  setConfig: setConfig
+};
 
 module.exports = visionectApi;
 
-var methods = require('./visionect-methods');
-var helper = require('./visionect-helper');
+var methods = require("./visionect-methods");
+var helper = require("./visionect-helper");
 
 function getDeviceList() {
-    return methods.visionectGetMethod('/api/device/');
+  return methods.visionectGetMethod("/api/device/");
 }
 
 function getDevice(uuid) {
-    return methods.visionectGetMethod('/api/device/' + uuid);
+  return methods.visionectGetMethod("/api/device/" + uuid);
 }
 
 function updateDevice(uuid, body) {
-    return methods.visionectPutMethod('/api/device/' + uuid, body, 204);
+  return methods.visionectPutMethod("/api/device/" + uuid, body, 204);
 }
 
 function updateDeviceList(bodyList) {
-    return methods.visionectPutMethod('/api/device/', bodyList, 204);
+  return methods.visionectPutMethod("/api/device/", bodyList, 204);
 }
 
 function removeDevice(id) {
-    return methods.visionectDeleteMethod('api/device/' + id, 204, 'application/json');
+  return methods.visionectDeleteMethod(
+    "api/device/" + id,
+    204,
+    "application/json"
+  );
 }
 
 function rebootDevice(id) {
-    return methods.visionectPostMethod('api/device/' + id, 204, 'application/json');
+  return methods.visionectPostMethod(
+    "api/device/" + id,
+    204,
+    "application/json"
+  );
 }
 
 function getSession(uuid) {
-    return methods.visionectGetMethod('/api/session/' + uuid);
+  return methods.visionectGetMethod("/api/session/" + uuid);
 }
 
 function getSessionList() {
-    return methods.visionectGetMethod('/api/session/');
+  return methods.visionectGetMethod("/api/session/");
 }
 
 function updateSession(uuid, body) {
-    return methods.visionectPutMethod('/api/session/' + uuid, body, 204);
+  return methods.visionectPutMethod("/api/session/" + uuid, body, 204);
 }
 
 function updateSessionList(body) {
-    return methods.visionectPutMethod('/api/session/', body, 204);
+  return methods.visionectPutMethod("/api/session/", body, 204);
 }
 
 function removeSession(id) {
-    return methods.visionectDeleteMethod('/api/session/' + id, 204, 'application/json');
+  return methods.visionectDeleteMethod(
+    "/api/session/" + id,
+    204,
+    "application/json"
+  );
 }
 
 function createSession(id, body) {
-    return methods.visionectPostMethod('/api/session/' + id, body, 201, 'application/json');
+  return methods.visionectPostMethod(
+    "/api/session/" + id,
+    body,
+    201,
+    "application/json"
+  );
 }
 
 function restartSession(id) {
-    return methods.visionectPostMethod('/api/session/' + id + '/restart', null, 201, 'application/json');
+  return methods.visionectPostMethod(
+    "/api/session/" + id + "/restart",
+    null,
+    201,
+    "application/json"
+  );
 }
 
 function restartSessionList(id, body) {
-    return methods.visionectPostMethod('/api/session/restart', body, 201, 'application/json');
+  return methods.visionectPostMethod(
+    "/api/session/restart",
+    body,
+    201,
+    "application/json"
+  );
 }
 
 function getUser(name) {
-    return methods.visionectGetMethod('/api/user/' + name);
+  return methods.visionectGetMethod("/api/user/" + name);
 }
 
 function getUserList() {
-    return methods.visionectGetMethod('/api/user/');
+  return methods.visionectGetMethod("/api/user/");
 }
 
 function updateUser(name, body) {
-    return methods.visionectPutMethod('/api/user/' + name, body, 204);
+  return methods.visionectPutMethod("/api/user/" + name, body, 204);
 }
 
 function updateUserList(body) {
-    return methods.visionectPutMethod('/api/user/', body, 201);
+  return methods.visionectPutMethod("/api/user/", body, 201);
 }
 
 function createUser(body) {
-    return methods.visionectPostMethod('/api/user/', body, 201, 'application/json');
+  return methods.visionectPostMethod(
+    "/api/user/",
+    body,
+    201,
+    "application/json"
+  );
 }
 function removeUser(username) {
-    return methods.visionectDeleteMethod('/api/user/' + username, 204, 'application/json');
+  return methods.visionectDeleteMethod(
+    "/api/user/" + username,
+    204,
+    "application/json"
+  );
 }
 
 function getVisionectConfig() {
-    return methods.visionectGetMethod('/api/config/');
+  return methods.visionectGetMethod("/api/config/");
 }
 
 function updateConfig(body) {
-    return methods.visionectPutMethod('/api/config/', body, 204);
+  return methods.visionectPutMethod("/api/config/", body, 204);
 }
 
 function getServerLiveView(uuid) {
-    return methods.visionectGetMethod('/api/live/device/' + uuid + '/cached.png', 200, 'image/png');
+  return methods.visionectGetMethod(
+    "/api/live/device/" + uuid + "/cached.png",
+    200,
+    "image/png"
+  );
 }
 
 function getDeviceLiveView(uuid) {
-    return methods.visionectGetMethod('/api/live/device/' + uuid + '/image.png', 200, 'image/png');
+  return methods.visionectGetMethod(
+    "/api/live/device/" + uuid + "/image.png",
+    200,
+    "image/png"
+  );
 }
 
 function getDeviceStatus(uuid, dateFrom, dateTo, group) {
-    return methods.visionectGetMethod('/api/devicestatus/' + uuid + '?from=' + dateFrom + '&to=' + dateTo + '&group=' + group);
+  return methods.visionectGetMethod(
+    "/api/devicestatus/" +
+      uuid +
+      "?from=" +
+      dateFrom +
+      "&to=" +
+      dateTo +
+      "&group=" +
+      group
+  );
 }
 
 function sendImage(uuid, imageFile) {
-    return methods.visionectSendImage(uuid, imageFile);
+  return methods.visionectSendImage(uuid, imageFile);
 }
 
 function setConfig(config) {
-    helper.setApiSecret(config.apiSecret);
-    helper.setApiKey(config.apiKey);
-    helper.setHost(config.host);
-    helper.setPort(config.port);
+  helper.setApiSecret(config.apiSecret);
+  helper.setApiKey(config.apiKey);
+  helper.setHost(config.host);
+  helper.setPort(config.port);
+  helper.setProtocol(config.protocol || "http");
 }
--- visionect-methods.js
+++ visionect-methods.js
@@ -1,203 +1,229 @@
 var visionectMethods = {
-    visionectGetMethod: visionectGetMethod,
-    visionectPutMethod: visionectPutMethod,
-    visionectPostMethod: visionectPostMethod,
-    visionectDeleteMethod: visionectDeleteMethod,
-    visionectSendImage: visionectSendImage,
-    visionectGetImageMethod : visionectGetImageMethod
-}
+  visionectGetMethod: visionectGetMethod,
+  visionectPutMethod: visionectPutMethod,
+  visionectPostMethod: visionectPostMethod,
+  visionectDeleteMethod: visionectDeleteMethod,
+  visionectSendImage: visionectSendImage,
+  visionectGetImageMethod: visionectGetImageMethod
+};
 
 module.exports = visionectMethods;
 
-var helper = require('./visionect-helper'),
-    http = require('http'),
-    util = require('util'),
-    FormData = require('form-data'),
-    fs = require('fs');
+var helper = require("./visionect-helper"),
+  http = require("http"),
+  util = require("util"),
+  FormData = require("form-data"),
+  fs = require("fs");
 
 function visionectRequest(path, method, contentType, body, expectedHttpCode) {
-    var method = method ? method : 'GET',
-        contentType = contentType ? contentType : 'application/json',
-        expectedHttpCode = expectedHttpCode ? expectedHttpCode : 200,
-        date = Date(),
-        // strip get arguments from path
-        cleanPath = (path.indexOf('?') > -1 ) ? path.substr(0, path.indexOf('?')) : path;
+  var method = method ? method : "GET",
+    contentType = contentType ? contentType : "application/json",
+    expectedHttpCode = expectedHttpCode ? expectedHttpCode : 200,
+    date = Date(),
+    // strip get arguments from path
+    cleanPath =
+      path.indexOf("?") > -1 ? path.substr(0, path.indexOf("?")) : path;
 
-    var authorization = helper.getAuthorization(cleanPath, method, contentType, date);
+  var authorization = helper.getAuthorization(
+    cleanPath,
+    method,
+    contentType,
+    date
+  );
 
-    var headers = {
-        'content-type': contentType,
-        'Date': date,
-        'Authorization': authorization
-    };
+  var headers = {
+    "content-type": contentType,
+    Date: date,
+    Authorization: authorization
+  };
 
-    body = body ? JSON.stringify(body) : null;
+  body = body ? JSON.stringify(body) : null;
 
-    var request = http.request({
-        method: method,
-        host: helper.getHost(),
-        port: helper.getPort(),
-        path: path,
-        headers: headers,
-        encoding: 'binary'
+  var request = http.request({
+    method: method,
+    host: helper.getHost(),
+    port: helper.getPort(),
+    protocol: helper.getProtocol(),
+    path: path,
+    headers: headers,
+    encoding: "binary"
+  });
+
+  console.log(
+    method,
+    helper.getProtocol(),
+    helper.getHost(),
+    helper.getPort(),
+    path,
+    body,
+    authorization,
+    date
+  );
+
+  var promise = new Promise(function(resolve, reject) {
+    // reject any request errors
+    request.on("error", function(err) {
+      reject(err);
     });
 
-    console.log(method, helper.getHost(), helper.getPort(), path, body, authorization, date);
+    // if request times out, emit an error and abort the request
+    request.on("timeout", function() {
+      request.emit("error", new Error("ETIMEDOUT"));
+      request.abort();
+    });
 
-    var promise = new Promise(
-        function (resolve, reject) {
+    request.on("response", function(response) {
+      response.setEncoding("binary");
+      var responseBody = "";
+      if (response.statusCode != expectedHttpCode) {
+        resolve("Error: " + response.statusCode);
+      }
+      response.on("data", function(chunk) {
+        responseBody += chunk;
+      });
+      response.on("end", function() {
+        resolve(responseBody);
+      });
 
-            // reject any request errors
-            request.on("error",function (err) {
-                reject(err);
-            });
+      // reject any response errors
+      response.on("error", function(err) {
+        reject(err);
+      });
+    });
+  });
 
-            // if request times out, emit an error and abort the request
-            request.on('timeout', function () {
-                request.emit("error",new Error('ETIMEDOUT'));
-                request.abort();
-            });
+  // write the body and send the request after we are attached to events
+  request.end(body);
 
-            request.on('response', function (response) {
-                response.setEncoding('binary');
-                var responseBody = '';
-                if (response.statusCode != expectedHttpCode) {
-                    resolve(('Error: ' + response.statusCode));
-                }
-                response.on('data', function (chunk) {
-                    responseBody += chunk;
-                });
-                response.on('end', function () {
-                    resolve(responseBody);
-                });
-
-                // reject any response errors
-                response.on('error', function (err) {
-                    reject(err);
-                });
-            });
-        }
-    );
-
-    // write the body and send the request after we are attached to events
-    request.end(body);
-
-    return promise;
-
+  return promise;
 }
 
 function visionectSendImage(uuid, imageFile) {
-    var imageStream = fs.createReadStream(imageFile);
+  var imageStream = fs.createReadStream(imageFile);
 
-    var form = new FormData();
-    form.append('image', imageStream);
-    var headers = form.getHeaders(),
-        date = Date(),
-        path = util.format('/backend/%s', uuid),
-        method = 'PUT';
-    headers.Date = date;
-    headers.Authorization = helper.getAuthorization(path, method, headers['content-type'], date);
+  var form = new FormData();
+  form.append("image", imageStream);
+  var headers = form.getHeaders(),
+    date = Date(),
+    path = util.format("/backend/%s", uuid),
+    method = "PUT";
+  headers.Date = date;
+  headers.Authorization = helper.getAuthorization(
+    path,
+    method,
+    headers["content-type"],
+    date
+  );
 
-    console.log(helper.getHost());
-    var request = http.request({
-        method: method,
-        host: helper.getHost(),
-        port: helper.getPort(),
-        path: path,
-        headers: headers
+  console.log(helper.getHost());
+  var request = http.request({
+    method: method,
+    host: helper.getHost(),
+    port: helper.getPort(),
+    protocol: helper.getProtocol(),
+    path: path,
+    headers: headers
+  });
+
+  // console.log(util.inspect(form, {showHidden: false, depth: null}));
+
+  console.log("uuid : ", uuid);
+  console.log("host : ", helper.getHost());
+  console.log("content-type : ", headers["content-type"]);
+  console.log("apiKey : ", helper.getApiKey());
+  console.log("apiSecret : ", helper.getApiSecret());
+  console.log(
+    "requete url : ",
+    helper.getProtocol() + "://" + helper.getHost() + ":" + "8081" + path
+  );
+  console.log(
+    "headers : ",
+    util.inspect(headers, { showHidden: false, depth: null })
+  );
+
+  var promise = new Promise(function(resolve, reject) {
+    form.pipe(request);
+
+    request.on("response", function(res) {
+      var body = "";
+      if (res.statusCode != 200) {
+        reject("Error: " + res.statusCode);
+      }
+      imageStream.on("error", function(err) {
+        res.end("error imageStream : ", err);
+      });
+      res.on("data", function(chunk) {
+        console.log("chocolate ", chunk);
+        body += chunk;
+      });
+      res.on("end", function() {
+        console.log("end : " + body);
+        resolve(body);
+      });
     });
-
-    // console.log(util.inspect(form, {showHidden: false, depth: null}));
-
-    console.log('uuid : ', uuid);
-    console.log('host : ', helper.getHost());
-    console.log('content-type : ', headers['content-type']);
-    console.log('apiKey : ', helper.getApiKey());
-    console.log('apiSecret : ', helper.getApiSecret());
-    console.log('requete url : ', helper.getHost() + ':' + '8081' + path);
-    console.log('headers : ', util.inspect(headers, {showHidden: false, depth: null}));
-
-    var promise = new Promise(
-        function (resolve, reject) {
-
-            form.pipe(request);
-
-            request.on('response', function (res) {
-                var body = '';
-                if (res.statusCode != 200) {
-                    reject('Error: ' + res.statusCode)
-                }
-                imageStream.on('error', function (err) {
-                    res.end('error imageStream : ', err);
-                });
-                res.on('data', function (chunk) {
-                    console.log('chocolate ',chunk);
-                    body += chunk;
-                });
-                res.on('end', function () {
-                    console.log('end : ' + body);
-                    resolve(body);
-                });
-            });
-        }
-    );
-    return promise;
+  });
+  return promise;
 }
 
 function visionectGetMethod(path, expectedHttpCode, contentType) {
-    return visionectRequest(path, 'GET', contentType, null, expectedHttpCode)
+  return visionectRequest(path, "GET", contentType, null, expectedHttpCode);
 }
 
 function visionectPutMethod(path, body, expectedHttpCode, contentType) {
-    return visionectRequest(path, 'PUT', contentType, body, expectedHttpCode);
+  return visionectRequest(path, "PUT", contentType, body, expectedHttpCode);
 }
 
 function visionectPostMethod(path, body, expectedHttpCode, contentType) {
-    return visionectRequest(path, 'POST', contentType, body, expectedHttpCode);
+  return visionectRequest(path, "POST", contentType, body, expectedHttpCode);
 }
 
 function visionectDeleteMethod(path, expectedHttpCode, contentType) {
-    return visionectRequest(path, 'DELETE', contentType, null, expectedHttpCode);
+  return visionectRequest(path, "DELETE", contentType, null, expectedHttpCode);
 }
-function visionectGetImageMethod(path,expectedHttpCode,contentType){
-    var method = method ? method : 'GET',
-        contentType = contentType ? contentType : 'application/json',
-        expectedHttpCode = expectedHttpCode ? expectedHttpCode : 200,
-        date = Date(),
-        // strip get arguments from path
-        cleanPath = (path.indexOf('?') > -1 ) ? path.substr(0, path.indexOf('?')) : path;
+function visionectGetImageMethod(path, expectedHttpCode, contentType) {
+  var method = method ? method : "GET",
+    contentType = contentType ? contentType : "application/json",
+    expectedHttpCode = expectedHttpCode ? expectedHttpCode : 200,
+    date = Date(),
+    // strip get arguments from path
+    cleanPath =
+      path.indexOf("?") > -1 ? path.substr(0, path.indexOf("?")) : path;
 
-    var authorization = helper.getAuthorization(cleanPath, method, contentType, date);
+  var authorization = helper.getAuthorization(
+    cleanPath,
+    method,
+    contentType,
+    date
+  );
 
-    var headers = {
-        'content-type': contentType,
-        'Date': date,
-        'Authorization': authorization
-    };
+  var headers = {
+    "content-type": contentType,
+    Date: date,
+    Authorization: authorization
+  };
 
+  var options = {
+    method: method,
+    protocol: helper.getProtocol(),
+    host: helper.getHost(),
+    port: helper.getPort(),
+    path: path,
+    headers: headers
+  };
 
-    var options = {
-        method: method,
-        host: helper.getHost(),
-        port: helper.getPort(),
-        path: path,
-        headers: headers
-    };
+  http.get(options, function(res) {
+    var imagedata = "";
+    res.setEncoding("binary");
 
-    http.get(options, function(res){
-        var imagedata = ''
-        res.setEncoding('binary');
+    res.on("data", function(chunk) {
+      imagedata += chunk;
+    });
 
-        res.on('data', function(chunk){
-            imagedata += chunk
-        })
-
-        res.on('end', function(){
-            fs.writeFile('logo.png', imagedata, 'binary', function(err){
-                if (err) throw err
-                console.log('File saved.')
-            })
-        })
-
-    })
+    res.on("end", function() {
+      fs.writeFile("logo.png", imagedata, "binary", function(err) {
+        if (err) throw err;
+        console.log("File saved.");
+      });
+    });
+  });
 }
